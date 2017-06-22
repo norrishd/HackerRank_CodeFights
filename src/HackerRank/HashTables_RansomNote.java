d57fe049801bd9baa763ae5e42a4fa3e621ea951
David Norrish <u4815128@anu.edu.au>
2017-06-22 11:59:58 +1000

    Added Java docstrings

--- src/HackerRank/HashTables_RansomNote.java
+++ src/HackerRank/HashTables_RansomNote.java
@@ -6,6 +6,22 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+/**
+ * A kidnapper wrote a ransom note but is worried it will be traced back to him. He found a magazine and wants to know
+ * if he can cut out whole words from it and use them to create an untraceable replica of his ransom note.
+ * The words in his note are case-sensitive and he must use whole words available in the magazine, meaning he cannot
+ * use substrings or concatenation to create the words he needs.
+
+ Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note
+ exactly using whole words from the magazine; otherwise, print No.
+
+ Input Format
+
+ The first line contains two space-separated integers describing the respective values of m (the number of words
+ in the magazine) and n (the number of words in the ransom note).
+ The second line contains m space-separated strings denoting the words present in the magazine.
+ The third line contains n space-separated strings denoting the words present in the ransom note.
+ */
 public class HashTables_RansomNote {
 
     public static void main(String[] args) {
